b049d54e8f3a045afade8d16311c7c96899fa0ae
Rowin Ruizendaal <rowin_ruizendaal@hotmail.com>
2020-05-08 15:55:01 +0200

    Back-end dev express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/about', (req, res) => res.send('This is the about page'))
+app.get('/contact', (req, res) => res.send('This is the contact page'))
+app.get('*', (req, res) => res.send('ERROR 404'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
