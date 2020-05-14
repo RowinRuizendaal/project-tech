0858edbd7b3f2478fe962d017d4c2336214460a0
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2020-05-14 13:19:47 +0200

    updated with new exercise

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const app = express()
 const port = 3000
 
 
+var data = [
+    {
+      id: 'evil-dead',
+      title: 'Evil Dead',
+      plot: 'Five friends travel to a cabin in the woods, where they unknowingly release flesh-possessing demons.',
+      description: 'Five friends head to a remote cabin, where the discovery of a Book of the Dead leads them to unwittingly summon up demons living in the nearby woods. The evil presence possesses them until only one is left to fight for survival.'
+    },
+    {
+      id: 'the-shawshank-redemption',
+      title: 'The Shawshank Redemption',
+      plot: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+      description: 'Andy Dufresne is a young and successful banker whose life changes drastically when he is convicted and sentenced to life imprisonment for the murder of his wife and her lover. Set in the 1940’s, the film shows how Andy, with the help of his friend Red, the prison entrepreneur, turns out to be a most unconventional prisoner.'
+    }
+  ]
 
 
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+
 
 
 
@@ -26,6 +41,15 @@ app.get('/registeren', (req, res) => {
 })
 
 
+app.get('/mp3', (req, res) => {
+    res.sendFile(__dirname + '/Cool-Song.mp3');
+})
+
+
+app.get('/new', (req, res) => {
+    res.render('new.ejs', {data: data})
+})
+
 
 app.get('*', (req, res) => {
     res.render('not-found.ejs');
@@ -33,14 +57,12 @@ app.get('*', (req, res) => {
 
 
 
+
+
+
 // app.get('/about', (req, res) => res.send('This is the about page'))
 // app.get('/contact', (req, res) => res.send('This is the contact page'))
 
-// app.get('/index', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-//     const url = req.headers.host + '/' + req.url; //Logging the url we went to
-//     console.log(url);
-// })
 
 
 //http://localhost:3000/users/:2/books/:3 returns data in json format 
