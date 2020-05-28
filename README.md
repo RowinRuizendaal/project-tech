0cca5853f25b197277e9871c2f134eba69d7b4ef
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2020-05-28 16:40:56 +0200

    Added login feature/ register feature/ password validation/ user validation working I will be soon working on sessions

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-# project-tech
-
-Hi, welcome to my repository here you can find my source code for my dating app feature! I will be making a register and login feature. My feature will be programmed in node js.
-
-Static website can be found at:
-https://rowinruizendaal.github.io/project-tech/static-website/index.html
-
-Read my documentation pages here:
-https://github.com/RowinRuizendaal/project-tech/wiki
-
-
 # project tech
 
-> A dating app feature
+> > Hi, welcome to my repository here you can find my source code for my dating app feature! I will be making a register and login feature. My feature will be programmed in node js.
 
 ## Build Setup
 
@@ -29,4 +18,21 @@ npm install
 Nodemon index.js
 ```
 
-For a detailed explanation on my research please feel free to visit my wiki [wiki](https://github.com/RowinRuizendaal/project-tech/wiki).
+## Note:
+To run this project you will need my .env file, to get this either message me one of the options below or fill in my github issue template
+
+_Email:_
+_<rowin.ruizendaal@hva.nl>_
+
+_Contact me on cmd tech slack:_
+_[Slack](https://cmda-tech.slack.com/archives/D011V7V1L1K)_
+
+_Fill in github issue template:_
+_[Github Issue](https://github.com/RowinRuizendaal/project-tech/issues/new/choose)_
+
+_For a detailed explanation on my research please feel free to visit my wiki_ 
+_[wiki](https://github.com/RowinRuizendaal/project-tech/wiki)_
+
+_Static website_
+_[Static website](https://rowinruizendaal.github.io/project-tech/static-website/index.html)_
+
--- index.js
+++ index.js
@@ -12,25 +12,23 @@ let db = null;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
 
 
-mongodb.MongoClient.connect(uri, {
-  useUnifiedTopology: true},
-function(err, client) {
-  if (err) throw err;
-  db = client.db(process.env.DB_NAME);
-});
+mongodb.MongoClient.connect(uri, {useUnifiedTopology: true},
+    function(err, client) {
+      if (err) throw err;
+      db = client.db(process.env.DB_NAME);
+    });
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.post('/:add', add);
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.post('/add', register); // Register function
+app.post('/login', login); // Validate if user exist
 
 
 app.get('/index', (req, res) => {
   res.render('index.ejs');
-  const url = req.headers.host + req.url; // Logging the url we went to
-  console.log(url);
 });
 
 app.get('/inloggen', (req, res) => {
@@ -41,10 +39,12 @@ app.get('/registeren', (req, res) => {
   res.render('registeren.ejs');
 });
 
+app.get('/profile', (req, res) => {
+  res.render('profile.ejs');
+});
 
 app.get('/list', (req, res) => {
   db.collection('Users').find().toArray(push); // promise pending
-
 
   function push(err, data) {
     if (err) console.log(err);
@@ -53,21 +53,30 @@ app.get('/list', (req, res) => {
 });
 
 
-function add(req, res) {
-  db.collection('Users').insertOne({
-    'username': req.body.name,
-    'email': req.body.email,
-    'password': req.body.password,
-  });
-
-  console.log(`A new user has registered #awesome! : ${req.body.email}`);
-  res.redirect('/list');
+function register(req, res) {
+  if (req.body.password == req.body.password2) {
+    console.log('wachtwoorden overeen');
+    db.collection('Users').insertOne({
+      'username': req.body.name,
+      'email': req.body.email,
+      'password': req.body.password,
+    });
+    console.log(`A new user has registered #awesome! : ${req.body.email}`);
+    res.redirect('/list');
+  } else res.redirect('/registeren');
 }
 
 
-app.get('/detail', (req, res) => {
-  res.render('detail.ejs', {data: data});
-});
+function login(req, res) {
+  db.collection('Users').findOne({
+    email: req.body.email,
+    password: req.body.password,
+  }, function(error, response) {
+    if (error) throw error;
+    if (response) res.redirect('/profile'); // Adding session later
+    else res.redirect('/inloggen');
+  });
+};
 
 
 app.listen(port, () => console.log(`Dating-app listening at http://localhost:${port}`));
