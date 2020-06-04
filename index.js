7cb8052cf1a916ad37d0d11b0b12e86d1700cb3e
Rowin Ruizendaal <33514521+RowinRuizendaal@users.noreply.github.com>
2020-06-04 16:26:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 const objectId = mongodb.ObjectID;
 const flash = require('express-flash');
